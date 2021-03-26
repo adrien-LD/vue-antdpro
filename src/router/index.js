7ec2153c5d34e73b3a23df21a9c1b46b66dc9e98
adrien <18710735867@163.com>
2021-03-26 21:06:33 +0800

    feat: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [];
+const routes = [
+  {
+    path: '/user',
+    children: [
+      {
+        path: '/user/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+      },
+      {
+        path: '/user/register',
+        name: 'register',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+      },
+    ],
+  },
+  {
+    path: '/',
+    name: 'Home',
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  }];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
